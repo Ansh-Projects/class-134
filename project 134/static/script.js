36c29bfad58d29684b2726321ea26d2cd8579a18
Ansh-Projects <145835553+Ansh-Projects@users.noreply.github.com>
2023-11-03 19:09:15 +0530

    Add files via upload

---
+++ project 134/static/script.js
@@ -0,0 +1,76 @@
+
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Fetch the current date and update it in the DOM
+        let date_time = new Date()
+        let current_date = date_time.toLocaleDateString()
+
+        $('#date').text("Date :" + current_date)
+
+        
+        })
+
+
+
+    //  write an event, when Submit button is clicked
+    $('#button').click(function(){
+
+        //  get the text value from the textarea using the 'val()' method
+        let text_value = $('#text').val()
+
+        //  Convert it to JS object.
+        //  Provide a 'key' here and in write the same in app.py file as well to extract data
+        let input_text = {'customer_review' : text_value}
+        console.log(input_text)
+
+        //  ajax request
+        $.ajax({
+
+            //  type of web request
+            type : 'POST',
+
+            //  Data to be sent in JSON format
+            data : JSON.stringify(),
+
+            //  type of response expected is json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  if everything is successful, run this function
+            success : function(result){
+                let prediction = result.prediction
+                let emoji_url = result.url
+                console.log(emoji_url)
+
+                // extract prediction and emoticon url from result
+                predicted_emotion = result.date.predicted_emotion
+                emo_url = result.date.predicted_emotion_img_url
+
+                //  update the DOM elements
+                $("#prediction").html(predicted_emotion)
+                $('#prediction').css("display", "block");
+
+                $("#emo_img_url").attr('src', emo_url);
+                $('#emo_img_url').css("display", "block");
+            
+
+                //  show them
+
+            },
+
+            //  if any error, run this function
+            error : function(result){
+                
+                console.log(result)
+            }
+        })
+
+
+        //  clearing the textbox after every button push
+        $('#text').val("")
+    })
+        
